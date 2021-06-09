0bdb065ac37b25c2320e9226bf09c4a15d4ff9e4
h.kato <h.kato@cocolabo.jp>
2021-06-09 18:07:24 +0900

    add get find all

--- configs/routes.go
+++ configs/routes.go
@@ -36,5 +36,17 @@ func SetupRoutes(contactRepository *repositories.ContactRepository) *gin.Engine 
 		context.JSON(code, response)
 	})
 
+	route.GET("/", func(context *gin.Context) {
+		code := http.StatusOK
+
+		response := services.FindAllContacts(*contactRepository)
+
+		if !response.Success {
+			code = http.StatusBadRequest
+		}
+
+		context.JSON(code, response)
+	})
+
 	return route
 }
