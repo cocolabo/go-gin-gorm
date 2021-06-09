f356dda2dafc1803a5734188eaffdf9d8ac0144b
h.kato <h.kato@cocolabo.jp>
2021-06-09 14:34:14 +0900

    add gorm mysql driver

---
+++ database/connection.go
@@ -0,0 +1,12 @@
+package database
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() (*gorm.DB, error) {
+	dsn := "go:pass@tcp(127.0.0.1:53306)/go_app?charset=utf8mb4&parseTime=True&loc=Local"
+
+	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/cocolabo/go-gin-gorm/database"
+
+func main() {
+	database.Connect()
+}
